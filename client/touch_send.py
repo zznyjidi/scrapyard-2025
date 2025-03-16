6fd829137a360b0d69d8e2c1be92a17ff1daca08
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2025-03-16 17:03:08 -0400

    thread with lock

--- client/touch_send.py
+++ client/touch_send.py
@@ -1,4 +1,6 @@
-from machine import Pin, FPIOA, TOUCH
+from machine import Pin, FPIOA, TOUCH # type: ignore
+import time
+import _thread
 
 # Init Touch
 touch = TOUCH(0)
@@ -8,32 +10,50 @@ fpioa = FPIOA()
 fpioa.set_function(21, FPIOA.GPIO21)
 KEY = Pin(21, Pin.IN, Pin.PULL_UP)
 
-firstTouch = False
-upSent = False
-emptyTouchCount = 0
-buttonTriggered = False
+touchThreadLock = _thread.allocate_lock()
 
-while True:
-    touchEvents = touch.read()
-    if touchEvents:
-        emptyTouchCount = 0
+def touchThread(lock: _thread.LockType):
+    lock.acquire()
+
+    try:
+        firstTouch = False
         upSent = False
-        pos = (touchEvents[0].x, touchEvents[0].y)
-        print(f'x0={pos[0]} y0={pos[1]}')
-        if (not firstTouch):
-            print('DOWN')
-            firstTouch = True
-    else:
-        emptyTouchCount += 1
-        if emptyTouchCount > 10:
-            if not upSent:
-                print('UP')
-                upSent = True
-            firstTouch = False
-            emptyTouchCount = 0
-    if (KEY.value() == 0):
-        if (not buttonTriggered):
-            print('SWITCH')
-            buttonTriggered = True
-    else:
+        emptyTouchCount = 0
         buttonTriggered = False
+        
+        while True:
+            touchEvents = touch.read()
+            if touchEvents:
+                emptyTouchCount = 0
+                upSent = False
+                pos = (touchEvents[0].x, touchEvents[0].y)
+                print(f'x0={pos[0]} y0={pos[1]}')
+                if (not firstTouch):
+                    print('DOWN')
+                    firstTouch = True
+            else:
+                emptyTouchCount += 1
+                if emptyTouchCount > 10:
+                    if not upSent:
+                        print('UP')
+                        upSent = True
+                    firstTouch = False
+                    emptyTouchCount = 0
+            if (KEY.value() == 0):
+                if (not buttonTriggered):
+                    print('SWITCH')
+                    buttonTriggered = True
+            else:
+                buttonTriggered = False
+    finally:
+        lock.release()
+
+
+_thread.start_new_thread(touchThread, (touchThreadLock,))
+
+while not touchThreadLock.locked():
+    pass
+
+touchThreadLock.acquire()
+touchThreadLock.release()
+
